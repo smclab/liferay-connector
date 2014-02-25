761caf75a73d16c0354d51674d53e52051d6e941
Pier Paolo Ramon <ramonpierre@gmail.com>
2014-02-25 12:56:25 +0100

    Build and explicit Titanium support \o/

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,18 +2,75 @@ module.exports = function (grunt) {
 
 	grunt.initConfig({
 
+    pkg: grunt.file.readJSON('package.json'),
+
 		mochaTest: {
 			options: {
 				reporter: 'spec'
 			},
 			src: [ 'test/*.js' ]
-		}
+		},
+
+    clean: {
+      "modules": [ 'test/fake-titanium-app/modules' ],
+      "app": [ 'test/fake-titanium-app/build' ]
+    },
+
+    titaniumifier: {
+      "module": {}
+    },
+
+    titanium: {
+      "ios": {
+        options: {
+          command: 'build',
+          logLevel: 'info',
+          projectDir: './test/fake-titanium-app',
+          platform: 'ios'
+        }
+      },
+      "droid": {
+        options: {
+          command: 'build',
+          logLevel: 'trace',
+          projectDir: './test/fake-titanium-app',
+          platform: 'android',
+          deviceId: grunt.option('device-id')
+        }
+      }
+    },
+
+    unzip: {
+      "module": {
+        src: '<%= pkg.name %>-commonjs-<%= pkg.version %>.zip',
+        dest: 'test/fake-titanium-app'
+      }
+    }
 
 	});
 
+  grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-mocha-test')
+	grunt.loadNpmTasks('grunt-titanium')
+	grunt.loadNpmTasks('grunt-titaniumifier')
+  grunt.loadNpmTasks('grunt-zip');
 
 	grunt.registerTask('test:node', [ 'mochaTest' ]);
+
+  grunt.registerTask('setup-spec', function () {
+    grunt.file.copy('./test/spec.js', './test/fake-titanium-app/Resources/spec.js', {
+      process: function (source) {
+        return "\n\n/* DO NOT MODIFY THIS FILE! Work on test/spec.js instead! */\n\n" + source;
+      }
+    });
+  });
+
+  grunt.registerTask('build:titanium', [ 'titaniumifier:module' ]);
+  grunt.registerTask('test:ios', [ 'unzip:module', 'setup-spec', 'titanium:ios' ]);
+  grunt.registerTask('test:droid', [ 'unzip:module', 'setup-spec', 'titanium:droid' ]);
+
+  grunt.registerTask('ios', [ 'clean', 'build:titanium', 'test:ios' ]);
+  grunt.registerTask('droid', [ 'clean', 'build:titanium', 'test:droid' ]);
 
 	grunt.registerTask('default', [ 'test:node' ]);
 
